50bf774d1e4e3ee0f76fbfa231e4c1440771b6b9
harsh9539 <goyalh862@gmail.com>
2022-02-09 07:11:06 +0530

    Level 2 encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.set("view engine","ejs");
@@ -11,10 +12,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-}
+});
+
+const secret = "Thisisourlittlesecret";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 
 
 const User = new mongoose.model("User",userSchema);
